3001ee328df7213a70967bab3a868ea5abb6ab9c
JuanJoseAriasJaramillo <juanjoseariasj@hotmail.com>
2022-07-23 14:11:18 -0500

    calculadora funcional

--- OtroCurso/main.py
+++ OtroCurso/main.py
@@ -1,13 +1,16 @@
 import funcionesMatematicas
+from colorama import Fore, Back, Style, init
+init(autoreset=True)
 
-print("Calculadora hecha por JA")
-encendido = int(input("ingrese 1 si quiere usar la calculadora"))
+
+print(Fore.RED + "Calculadora hecha por JA")
+encendido = int(input(Back.WHITE + "ingrese 1 si quiere usar la calculadora"))
 pregunta = 1
 if encendido == 1:
     while pregunta == 1:
-        n1 = int(input("Ingrese el primer numero"))
-        n2 = int(input("Ingrese el segundo numero"))
-        decision = input("Puedes usar suma, resta, multiplicacion, "
+        n1 = int(input(Fore.GREEN + "Ingrese el primer numero"))
+        n2 = int(input(Fore.LIGHTGREEN_EX + "Ingrese el segundo numero"))
+        decision = input(Style.BRIGHT + "Puedes usar suma, resta, multiplicacion, "
                          "division, potenciacion, es par utiliza sus respectivos "
                          "operadores logicos para escoger")
 
@@ -17,21 +20,19 @@ if encendido == 1:
             print(f"Resultado {resultado}")
             str(funcionesMatematicas.suma(n1, n2))
             print("Usando cambio de tipo a string para poder concatenar")
-            print("Resultado " + str(funcionesMatematicas.suma(n1, n2)))
+            print(Fore.LIGHTYELLOW_EX + Back.BLUE + "Resultado " + str(funcionesMatematicas.suma(n1, n2)))
         elif decision == '-':
-            print("Resultado " + str(funcionesMatematicas.resta(n1, n2)))
+            print(Fore.LIGHTYELLOW_EX + Back.BLUE + "Resultado " + str(funcionesMatematicas.resta(n1, n2)))
         elif decision == '*':
-            print("Resultado " + str(funcionesMatematicas.multiplicacion(n1, n2)))
+            print(Fore.LIGHTYELLOW_EX + Back.BLUE + "Resultado " + str(funcionesMatematicas.multiplicacion(n1, n2)))
         elif decision == '/':
-            print("Resultado " + str(funcionesMatematicas.division(n1, n2)))
+            print(Fore.LIGHTYELLOW_EX + Back.BLUE + "Resultado " + str(funcionesMatematicas.division(n1, n2)))
         elif decision == '**':
-            print("Resultado " + str(funcionesMatematicas.potenciacion(n1, n2)))
+            print(Fore.LIGHTYELLOW_EX + Back.BLUE + "Resultado " + str(funcionesMatematicas.potenciacion(n1, n2)))
         elif decision == '%':
             if funcionesMatematicas.esPar(n1, n2) == True:
-                print("Resultado es par")
+                print(Fore.LIGHTYELLOW_EX + Back.BLUE + "Resultado es par")
             else:
-                print("Resultado es inpar")
+                print(Fore.LIGHTYELLOW_EX + Back.BLUE + "Resultado es inpar")
 
-        pregunta = int(input("Ingrese 1 para seguir usando la calculadora"))
-
-
+        pregunta = int(input(Back.WHITE + "Ingrese 1 para seguir usando la calculadora"))
